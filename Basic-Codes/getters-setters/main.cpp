3e2bac2f933211ee914996a07632d846d1301ef5
Arpita Tugave <arpita.subash.tugave@gmail.com>
2016-11-07 14:19:00 -0500

    Create main.cpp

---
+++ Basic-Codes/getters-setters/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "Person.h"
+#include <string>
+
+using namespace std;
+
+int main() {
+
+	Person person;
+
+	person.setName("Geogia");
+
+	cout << person.toString() << endl;
+
+	cout << "Name of person with get method: " << person.getName() << endl;
+
+	return 0;
+}
