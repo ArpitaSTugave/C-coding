28513644d3c02d8d371845e059fea7ce424eef2f
Arpita Tugave <arpita.subash.tugave@gmail.com>
2016-11-09 16:52:30 -0500

    Create Cat1.cpp

---
+++ Basic-Codes/Namespace/Cat1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "Cat1.h"
+using namespace cats;
+using namespace std;
+
+Cat::Cat()
+{
+}
+
+
+Cat::~Cat()
+{
+}
+
+void Cat::speak() {
+	cout << "Meoooww!!!" << endl;
+}
