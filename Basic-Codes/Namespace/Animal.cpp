b30c5ac0a8a03155ec3bbe2f08fc7e0953685cbd
Arpita Tugave <arpita.subash.tugave@gmail.com>
2016-11-09 16:53:10 -0500

    Create Animal.cpp

---
+++ Basic-Codes/Namespace/Animal.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "Animal.h"
+using namespace anim;
+using namespace std;
+
+Cat::Cat()
+{
+}
+
+
+Cat::~Cat()
+{
+}
+
+void Cat::speak() {
+	cout << "Sssshhhh!!!" << endl;
+}
